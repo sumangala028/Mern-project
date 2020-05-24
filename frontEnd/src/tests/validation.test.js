a9c81f948ff51b104fadec961611516020c4b852
suthagar23 <suthagar.14@cse.mrt.ac.lk>
2020-05-24 21:07:47 +0530

    Added Jest tests

--- frontEnd/src/tests/validation.test.js
+++ frontEnd/src/tests/validation.test.js
@@ -1,32 +1,32 @@
 
 import { validateDiscount } from '../utils/validations';
 
-test('ValidateDiscount should return false if price and discount are null', () => {
+it('ValidateDiscount should return false if price and discount are null', () => {
   const response = validateDiscount("", "")
   expect(response).toBe(false);
 });
 
-test('ValidateDiscount should return false if price is  null', () => {
+it('ValidateDiscount should return false if price is  null', () => {
   const response = validateDiscount("", 100)
   expect(response).toBe(false);
 });
 
-test('ValidateDiscount should return false if discount is null', () => {
+it('ValidateDiscount should return false if discount is null', () => {
   const response = validateDiscount(100, "")
   expect(response).toBe(false);
 });
 
-test('ValidateDiscount should return true if price is greater than discount', () => {
+it('ValidateDiscount should return true if price is greater than discount', () => {
   const response = validateDiscount(100, 10)
   expect(response).toBe(true);
 });
 
-test('ValidateDiscount should return true if price is equal to discount', () => {
+it('ValidateDiscount should return true if price is equal to discount', () => {
   const response = validateDiscount(100, 100)
   expect(response).toBe(true);
 });
 
-test('ValidateDiscount should return false if price is less to discount', () => {
+it('ValidateDiscount should return false if price is less to discount', () => {
   const response = validateDiscount(100, 101)
   expect(response).toBe(false);
 });
